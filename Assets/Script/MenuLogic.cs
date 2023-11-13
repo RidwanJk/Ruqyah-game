de421deeabe39274c2b2beefb81e3cbf855f112a
asrulmaaliy26 <lostsaga06k@gmail.com>
2023-11-13 10:40:04 +0700

    fix menu

--- Assets/Script/MenuLogic.cs
+++ Assets/Script/MenuLogic.cs
@@ -1,13 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
 
 public class MenuLogic : MonoBehaviour
 {
-    public void pindahScene(string sceneName)
+    // Nama-nama panel
+    public GameObject panelUtama;
+    public GameObject panelOpsi;
+    public GameObject panelCredit;
+
+    // Metode untuk pindah ke scene
+    public void PindahScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    // Metode untuk menampilkan panel utama
+    public void TampilkanPanelUtama()
+    {
+        panelUtama.SetActive(true);
+        panelOpsi.SetActive(false);
+    }
+
+    // Metode untuk menampilkan panel opsi
+    public void TampilkanPanelOpsi()
+    {
+        panelUtama.SetActive(false);
+        panelOpsi.SetActive(true);
+    }
+    public void TampilkanPanelCredit()
+    {
+        panelUtama.SetActive(false);
+        panelOpsi.SetActive(true);
+    }
+    // Metode untuk keluar dari game
+    public void KeluarDariGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
